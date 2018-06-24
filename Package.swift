cda5be5ad9a71c38b3c3a89c26c1d41c9534b816
Gustavo Perdomo <gperdomor@gmail.com>
2018-06-23 22:21:04 -0400

    update dependencies

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "AsyncExtensions", targets: ["AsyncExtensions"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/async.git", from: "1.0.0-rc")
+        .package(url: "https://github.com/vapor-community/async.git", from: "1.0.0-rc")
     ],
     targets: [
         .target(name: "AsyncExtensions", dependencies: ["Async"]),
